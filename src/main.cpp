92d3ee4a53463277acc4a3de1de9bb3cf25eae0b
2Kerfur <fidotovm06@gmail.com>
2022-07-13 23:25:36 -0400

    Glfw + vulkan added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow * window = glfwCreateWindow(800, 600,
+            "OVRenderer", nullptr, nullptr);
+    std::cout << "OVRenderer started" << std::endl;
+
+    uint32_t extentionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+    printf("Extention count: %i\n", extentionCount);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+
 }
